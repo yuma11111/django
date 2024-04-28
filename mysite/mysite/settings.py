92dbe67c8850a70576f1777535dc7c980597b833
yuma11111 <72676007+yuma11111@users.noreply.github.com>
2024-04-28 17:54:13 +0900

    チュートリアル1〜3 途中まで

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#日本語に変更
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
---
+++ mysite/readme.txt
@@ -0,0 +1,6 @@
+ユーザ名：yuma
+メールアドレス：yuma.saturn@gmail.com
+パスワード：yuma1028
+
+管理画面：http://127.0.0.1:8000/admin/
+polls：http://127.0.0.1:8000/polls
