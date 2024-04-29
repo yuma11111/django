ec6c665ac7a8289e2b6e8e4bca425979b1c9ddd5
yuma11111 <72676007+yuma11111@users.noreply.github.com>
2024-04-29 16:44:52 +0900

    チュートリアル4完了

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,35 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    #モデル名を指定するか
+    model = Question
+    context_object_name = "latest_question_list"
+
+    """以下の処理でDBからのデータ一覧を取得すること
+    　　ただし、関数名は「get_queryset」でないとNG
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    """
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list, }
@@ -14,9 +40,24 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    response = "あなたは %s の質問の結果を見ています。"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/result.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("あなたは %s の質問に投票しています。" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request, 
+            "polls/detail.html",
+            {
+                "question": question, 
+                "error_message": "いずれかを選択してください。", 
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
 
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
