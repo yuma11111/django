5330e5d355be6cadfffacf5092845385a6d4258f
yuma11111 <72676007+yuma11111@users.noreply.github.com>
2024-04-29 14:14:23 +0900

    チュートリアル3完了

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-#from django.template import loader
 
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    #template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list, }
     return render(request, "polls/index.html", context)
 
